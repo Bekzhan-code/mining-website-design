4a697f3fd5776dbf1d4012d585ed18b95babdf50
Aldashov Bekzhan <aldashov.bekzhan@gmail.com>
2023-09-30 00:04:40 +0600

    Верстка страниц About, Support, FAQ и стр. аутентификации

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// HEADER BURGER
 const headerBurger = document.querySelector(".header__burger");
 const headerNav = document.querySelector(".header__nav");
 
@@ -5,4 +6,25 @@ headerBurger.addEventListener("click", () => {
   headerBurger.classList.toggle("active");
   headerNav.classList.toggle("active");
   document.body.classList.toggle("lock");
+});
+
+// HEADER NAVIGATION
+const navLinks = document.querySelectorAll(".header__nav-item");
+const windowPathname = window.location.pathname;
+
+navLinks.forEach((link) => {
+  const linkPathname = new URL(link.href).pathname;
+
+  if (windowPathname === linkPathname) {
+    link.classList.add("active");
+  }
+});
+
+// FAQ question expanding
+const questions = document.querySelectorAll(".faq__item");
+
+questions.forEach((q) => {
+  q.addEventListener("click", () => {
+    q.classList.toggle("active");
+  });
 });
