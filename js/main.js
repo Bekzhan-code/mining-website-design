1c61efeab9435031502e5c4da3b2528d1150807a
Aldashov Bekzhan <aldashov.bekzhan@gmail.com>
2023-10-01 02:00:14 +0600

    Исправил радиусы калькулятора, размеры контактов и добавил скрытие/показ пароля

--- js/main.js
+++ js/main.js
@@ -28,3 +28,30 @@ questions.forEach((q) => {
     q.classList.toggle("active");
   });
 });
+
+// PASSWORD INPUT
+const togglePasswordImg = document.querySelector("#toggle-password-img");
+const passwordInput = document.querySelector("#password-input");
+
+const togglePasswordConfirmImg = document.querySelector(
+  "#toggle-confirm-password-img"
+);
+const passwordConfirmInput = document.querySelector("#password-confirm-input");
+
+const togglePassword = (pasInput, pasImg) => {
+  const type =
+    pasInput.getAttribute("type") === "password" ? "text" : "password";
+  pasInput.setAttribute("type", type);
+
+  pasImg.src = pasImg.src.includes("close")
+    ? pasImg.src.replace("close", "open")
+    : pasImg.src.replace("open", "close");
+};
+
+togglePasswordImg.addEventListener("click", () => {
+  togglePassword(passwordInput, togglePasswordImg);
+});
+
+togglePasswordConfirmImg.addEventListener("click", () => {
+  togglePassword(passwordConfirmInput, togglePasswordConfirmImg);
+});
